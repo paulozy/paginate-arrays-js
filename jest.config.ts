ede5026d7fb7f8760984f074a5472cce4174b798
Paulo Abreu <paulo.santana@luby.software>
2023-05-05 00:14:22 -0300

    chore(tests): 🛠️  add jest config

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  transform: {
+    "^.+\\.[t|j]sx?$": "ts-jest",
+  },
+};
