589da281e0b33fc34ecdf775a25a5abd309e800a
Paulo Abreu <paulo.santana@luby.software>
2023-05-07 00:23:59 -0300

    test(app tests): 🧪 ensure thats be possible paginate an array with before param

--- tests/cursor-paginate.spec.ts
+++ tests/cursor-paginate.spec.ts
@@ -30,32 +30,44 @@ describe("cursor paginate", () => {
 
   it("should be possible paginate an array with a cursor and after", () => {
     const perPage = 10;
-
     const identifier = "id";
-
     const after = data[10].id;
 
     const { data: paginatedItems, pagination } = cursorPaginate({
       data,
-
       perPage,
-
       identifier,
-
       after,
     });
 
     expect(paginatedItems).toHaveLength(perPage);
-
     expect(pagination).toMatchObject({
       hasPrevPage: true,
-
       hasNextPage: true,
-
       startCursor: paginatedItems[0].id,
-
       endCursor: paginatedItems[paginatedItems.length - 1].id,
+      totalPages: 10,
+    });
+  });
 
+  it("should be possible paginate an array with a cursor and before", () => {
+    const perPage = 10;
+    const identifier = "id";
+    const before = data[10].id;
+
+    const { data: paginatedItems, pagination } = cursorPaginate({
+      data,
+      perPage,
+      identifier,
+      before,
+    });
+
+    expect(paginatedItems).toHaveLength(perPage);
+    expect(pagination).toMatchObject({
+      hasPrevPage: true,
+      hasNextPage: true,
+      startCursor: paginatedItems[0].id,
+      endCursor: paginatedItems[paginatedItems.length - 1].id,
       totalPages: 10,
     });
   });
